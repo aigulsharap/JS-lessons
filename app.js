447c47348f4714c0d60a47c620b473d9c65a792d
aigulsharap <sharapshop@yandex.ru>
2026-05-10 18:26:28 +0300

    Изучили условные операторы if else

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const money = 100;
+const canBuy = money >= 50;
+
+if (canBuy) {
+    console.log('Может купить наш курс');
+} else if (money >= 5) {
+    console.log('Может купить мини курс')
+} else {
+    console.log ('Недостаточно средств для покупки');
+};
