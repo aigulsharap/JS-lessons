9149c1ff2efcfc29de6a85f14679b99e65c2b7ef
aigulsharap <sharapshop@yandex.ru>
2026-05-10 16:57:59 +0300

    Вторая попытка отправить домашнее задание

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const addressLat = 6;
 const addressLong = 7;
-const positionLat = 3;
+const positionLat = 13;
 const positionLong = 10;
 const distance = Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2);
 console.log(distance);
