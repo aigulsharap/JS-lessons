e4dc20eab4a133141562e83ecdfff541100f81ac
aigulsharap <sharapshop@yandex.ru>
2026-05-10 16:51:05 +0300

    Решила задачу про поиск расстояния между точками

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello world')
+const addressLat = 6;
+const addressLong = 7;
+const positionLat = 3;
+const positionLong = 10;
+const distance = Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2);
+console.log(distance);
